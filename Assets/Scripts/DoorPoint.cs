095ade15cccd42db17bcc213e260a339680481eb
SGalindo831 <galindo.sabino831@gmail.com>
2024-11-11 12:19:39 -0800

    Changed the Dungeon GGeneration script

--- Assets/Scripts/DoorPoint.cs
+++ Assets/Scripts/DoorPoint.cs
@@ -2,52 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class DoorPoint : MonoBehaviour
+{
+    public DoorDirection direction;
+    public bool isConnected = false;
+    public bool optional = false;  // Whether this door must be connected
+    public GameObject doorBlocker;
+
+    public void SealDoorway()
+    {
+        if (doorBlocker != null)
+        {
+            doorBlocker.SetActive(true);
+        }
+    }
+
+    public void OpenDoorway()
+    {
+        if (doorBlocker != null)
+        {
+            doorBlocker.SetActive(false);
+        }
+    }
+}
+
 public enum DoorDirection
 {
     North,
     South,
     East,
     West
-}
-
-public class DoorPoint : MonoBehaviour
-{
-    public DoorDirection direction;
-    public bool isConnected = false;
-    public GameObject doorBlockerPrefab; // Assign a wall prefab to block unused doors
-    private GameObject spawmedBlocker;
-
-    public void SealDoorway()
-    {
-        if (doorBlockerPrefab != null && !isConnected)
-        {
-            // Spawn the blocker with the correct rotation based on direction
-            Quaternion rotation = Quaternion.identity;
-            switch (direction)
-            {
-                case DoorDirection.North:
-                    rotation = Quaternion.Euler(0, 0, 0);
-                    break;
-                case DoorDirection.South:
-                    rotation = Quaternion.Euler(0, 180, 0);
-                    break;
-                case DoorDirection.East:
-                    rotation = Quaternion.Euler(0, 90, 0);
-                    break;
-                case DoorDirection.West:
-                    rotation = Quaternion.Euler(0, 270, 0);
-                    break;
-            }
-
-            spawmedBlocker = Instantiate(doorBlockerPrefab, transform.position, rotation, transform);
-        }
-    }
-
-    public void UnSealDoorway()
-    {
-        if (spawmedBlocker != null)
-        {
-            Destroy(spawmedBlocker);
-        }
-    }
 }
